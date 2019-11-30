36d701344c3832cf435cebdf557cf936ade34369
MarinaYsn <marinaysn@gmail.com>
2019-11-30 09:16:39 -0500

    Tower of Hanoi

---
+++ DailyChallenges/L04-Hard/TowerOfHanoi.js
@@ -0,0 +1,50 @@
+
+//Solution 1
+function towerHanoi(discs) {
+    console.log(Math.pow(2, discs) -1)
+    return Math.pow(2, discs) -1
+}
+towerHanoi(1) //, 1)
+towerHanoi(2) //, 3)
+towerHanoi(3) //, 7)
+towerHanoi(4) //, 15)
+towerHanoi(5) //, 31)
+towerHanoi(6) //, 63)
+towerHanoi(7) //, 127)
+towerHanoi(8)//, 255)
+towerHanoi(9)//, 511)
+towerHanoi(19)//, 524287)
+towerHanoi(13)//, 8191)
+towerHanoi(0)//, 0)
+
+//Solution 2
+function towerHanoi2(discs) {
+    if (discs === 0 || discs === 1) {
+        console.log(discs)
+		return discs
+    }    
+    if (discs === 2) {
+        console.log(3)
+		return 3
+    }
+    
+	 let total = 7;
+	for ( let i = 0, j = 1; i < discs-3; i++, j*=2){
+        total += 8*j
+    }
+    console.log(total)
+	return total
+}
+
+towerHanoi2(1) //, 1)
+towerHanoi2(2) //, 3)
+towerHanoi2(3) //, 7)
+towerHanoi2(4) //, 15)
+towerHanoi2(5) //, 31)
+towerHanoi2(6) //, 63)
+towerHanoi2(7) //, 127)
+towerHanoi2(8)//, 255)
+towerHanoi2(9)//, 511)
+towerHanoi2(19)//, 524287)
+towerHanoi2(13)//, 8191)
+towerHanoi2(0)//, 0)
