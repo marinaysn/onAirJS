21575edde79da1724b61eaaccf8d9dbd69e85651
MarinaYsn <marinaysn@gmail.com>
2019-11-30 09:26:17 -0500

    Tower of Hanoi

--- DailyChallenges/L04-Hard/TowerOfHanoi.js
+++ DailyChallenges/L04-Hard/TowerOfHanoi.js
@@ -1,4 +1,11 @@
+/*
+Example: There are three towers. The objective of the game is to move all the disks over to tower #3, but you can't place a larger disk onto a smaller disk.
 
+Tower of Hanoi
+
+Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
+
+*/
 //Solution 1
 function towerHanoi(discs) {
     console.log(Math.pow(2, discs) -1)
